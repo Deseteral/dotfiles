2ec6b220f1808bea7a3bc1f42ac2f601574af167
Deseteral <deseteral@gmail.com>
2024-02-23 19:15:58 +0100

    Keybind fix

--- nvim/lua/deseteral/plugins/lsp.lua
+++ nvim/lua/deseteral/plugins/lsp.lua
@@ -39,10 +39,10 @@ return {
         })
 
         -- Global mappings.
-        vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+        vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
         vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
         vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-        vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
         -- Current buffer mappings.
         vim.api.nvim_create_autocmd('LspAttach', {
