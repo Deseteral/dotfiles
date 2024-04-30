3630ce9473b4cdb662a0d4af2a0256bdc82da7d6
Deseteral <deseteral@gmail.com>
2024-04-30 20:33:43 +0200

    nvim: Additional color schemes

--- fragments/nvim/config/nvim/lua/deseteral/plugins/colorschemes.lua
+++ fragments/nvim/config/nvim/lua/deseteral/plugins/colorschemes.lua
@@ -1,11 +1,20 @@
 return {
-    'crispybaccoon/evergarden',
-    config = function()
-        require('evergarden').setup({
-            transparent_background = false,
-            contrast_dark = 'hard',
-        })
-
-        vim.cmd.colorscheme('evergarden')
-    end,
+    {
+        'crispybaccoon/evergarden',
+        config = function()
+            require('evergarden').setup({
+                transparent_background = false,
+                contrast_dark = 'hard',
+            })
+            -- vim.cmd.colorscheme('evergarden')
+        end,
+    },
+    {
+        "nyoom-engineering/oxocarbon.nvim",
+        config = function()
+            vim.opt.background = "dark"
+            vim.opt.background = "light"
+            vim.cmd.colorscheme("oxocarbon")
+        end
+    }
 }
