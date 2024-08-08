02e2887b3646744e4bd2d1439a880abd479295d1
Deseteral <deseteral@gmail.com>
2024-08-08 15:53:23 +0200

    Updated wezterm config

--- fragments/wezterm/.wezterm.lua
+++ fragments/wezterm/.wezterm.lua
@@ -25,6 +25,7 @@ config.show_new_tab_button_in_tab_bar = false
 config.show_tab_index_in_tab_bar = false
 config.tab_max_width = 32
 config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
 
 config.colors = {
     tab_bar = {
